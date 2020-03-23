689f40b3a401675747e5a49207780d8d99df8818
matiaz <matiaz@192.168.1.23>
2020-03-23 19:10:15 +0100

    Frame test

--- GameOfLife/src/gameOfLife/GameFrame.java
+++ GameOfLife/src/gameOfLife/GameFrame.java
@@ -1,11 +1,117 @@
 package gameOfLife;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
 	
-	private JPanel topPanel, bottomPanel, leftPanel, rightPanel, centerPanel;
-    private JSlider sliderJump, sliderZoom, sliderSpeed;
+	private JPanel topPanel, bottomPanel, leftPanel, rightPanel, righttopPanel, rightcenterPanel, centerPanel;
+    private JSlider jumpSlider, zoomSlider, speedSlider;
+    private JComboBox<String> modelsBox, languageBox;
+    private JButton chartButton, clearButton, ofonButton, stepButton, ruleButton;
+    
+    static final int JUMP_SLIDER_MIN = 0;
+    static final int JUMP_SLIDER_MAX = 100;
+    static final int JUMP_SLIDER_INIT = 0;
+    static final int ZOOM_SLIDER_MIN = 0;
+    static final int ZOOM_SLIDER_MAX = 100;
+    static final int ZOOM_SLIDER_INIT = 0;
+    static final int SPEED_SLIDER_MIN = 0;
+    static final int SPEED_SLIDER_MAX = 100;
+    static final int SPEED_SLIDER_INIT = 0;
+    
+    public GameFrame() {
+    	 this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+         this.setSize(900,600);
+         this.setLayout(new BorderLayout());
+         
+         //Top Panel
+         topPanel = new JPanel();
+         topPanel.setLayout(new FlowLayout());
+         
+         jumpSlider = new JSlider(JSlider.HORIZONTAL, JUMP_SLIDER_MIN, JUMP_SLIDER_MAX, JUMP_SLIDER_INIT);
+         jumpSlider.setPreferredSize(new Dimension(300,50));
+         jumpSlider.setMajorTickSpacing(20);
+         jumpSlider.setMinorTickSpacing(5);
+         jumpSlider.setPaintTicks(true);
+         jumpSlider.setPaintLabels(true);
+         jumpSlider.setName("SKOK");
+         
+         speedSlider = new JSlider(JSlider.HORIZONTAL, SPEED_SLIDER_MIN, SPEED_SLIDER_MAX, SPEED_SLIDER_INIT);
+         speedSlider.setPreferredSize(new Dimension(300,50));
+         speedSlider.setMajorTickSpacing(20);
+         speedSlider.setMinorTickSpacing(5);
+         speedSlider.setPaintTicks(true);
+         speedSlider.setPaintLabels(true);
+         
+         topPanel.add(speedSlider);
+         topPanel.add(jumpSlider);
+         this.add(topPanel, BorderLayout.PAGE_START);
+         
+         //Left Panel
+         leftPanel = new JPanel();
+         leftPanel.setLayout(new GridLayout(5,1));
+         
+         chartButton = new JButton("Wykres");
+         clearButton = new JButton("Czysc");
+         ofonButton = new JButton("START/STOP");
+         stepButton = new JButton("Nastepny");
+         ruleButton = new JButton("Zasady");
+         
+         leftPanel.add(chartButton);
+         leftPanel.add(ruleButton);
+         leftPanel.add(stepButton);
+         leftPanel.add(clearButton);
+         leftPanel.add(ofonButton);
+         this.add(leftPanel, BorderLayout.LINE_START);
+         
+         //Right Panel
+         rightPanel = new JPanel();
+         rightPanel.setLayout(new BorderLayout());
+         	//Right top Panel
+         righttopPanel = new JPanel();
+         languageBox = new JComboBox<String>();
+ 		 languageBox.setEditable(true);
+ 		 languageBox.setPreferredSize(new Dimension(70,30));
+ 		 languageBox.addItem("PL");
+ 		 
+ 		 righttopPanel.add(languageBox);
+ 		rightPanel.add(righttopPanel, BorderLayout.PAGE_START);
+         	//Right center Panel
+         rightcenterPanel = new JPanel();
+         zoomSlider = new JSlider(JSlider.VERTICAL, ZOOM_SLIDER_MIN, ZOOM_SLIDER_MAX, ZOOM_SLIDER_INIT);
+         zoomSlider.setPreferredSize(new Dimension(50,300));
+         zoomSlider.setMajorTickSpacing(20);
+         zoomSlider.setMinorTickSpacing(5);
+         zoomSlider.setPaintTicks(true);
+         zoomSlider.setPaintLabels(true);
+         
+         rightcenterPanel.add(zoomSlider);
+         rightPanel.add(rightcenterPanel, BorderLayout.CENTER);
+         
+         this.add(rightPanel, BorderLayout.LINE_END);
+         	
+
+         //Bottom Panel
+         bottomPanel = new JPanel();
+         modelsBox = new JComboBox<String>();
+ 		 modelsBox.setEditable(true);
+ 		 modelsBox.addItem("Brak");
+ 		 
+ 		 bottomPanel.add(modelsBox);
+ 		 this.add(bottomPanel, BorderLayout.PAGE_END);
+ 		 
+ 		 //Center Panel
+         centerPanel = new JPanel();
+         centerPanel.setBackground(Color.white);
+
+         this.add(centerPanel, BorderLayout.CENTER);
+    }
+    
+    public static void main(String[] args){
+        GameFrame frame = new GameFrame();
+        frame.setVisible(true);
+    }
     
 
 }
