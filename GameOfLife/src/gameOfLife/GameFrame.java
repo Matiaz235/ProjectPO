12b63a8f5d20e3380babc0249feecd4007e484a8
matiaz <matiaz@192.168.1.23>
2020-03-23 17:27:44 +0100

    Beginning

---
+++ GameOfLife/src/gameOfLife/GameFrame.java
@@ -0,0 +1,11 @@
+package gameOfLife;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+	
+	private JPanel topPanel, bottomPanel, leftPanel, rightPanel, centerPanel;
+    private JSlider sliderJump, sliderZoom, sliderSpeed;
+    
+
+}
